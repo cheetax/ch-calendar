c2ca20af511f94915bd9268209ad49d3a6bf1971
cheetax <dmitriy.grebenev@gmail.com>
2018-09-10 17:15:29 +0300

    111

--- src/Svg.js
+++ src/Svg.js
@@ -12,6 +12,9 @@ const SvgMinus = () => <Svg ><path d="M19 13H5v-2h14v2z" /></Svg>
 
 const SvgCalendar = () => <Svg ><path d="M17 12h-5v5h5v-5zM16 1v2H8V1H6v2H5c-1.11 0-1.99.9-1.99 2L3 19c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-1V1h-2zm3 18H5V8h14v11z" /></Svg>
 
+const SvgArrowLeft = () => <Svg><path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" /></Svg>
+
+
 
 export {
     SvgPlus,
